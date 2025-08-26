0e93615fca7e650df9f8a8c13948a383cf3916e9
Arjun.Raf <arjunhornet@gmail.com>
2025-08-26 17:32:14 +0200

    Workflow complete. MVP is up and running

--- main.py
+++ main.py
@@ -16,33 +16,117 @@ class Game:
     lives = 6
     wordLen = 0
     word2guess = ""
+    progress = ""
+    past_guesses = []
+    HANGMAN_PICS = [
+                    """
+                      +---+
+                      |   |
+                          |
+                          |
+                          |
+                          |
+                    =========
+                    """,
+                    """
+                      +---+
+                      |   |
+                      O   |
+                          |
+                          |
+                          |
+                    =========
+                    """,
+                    """
+                      +---+
+                      |   |
+                      O   |
+                      |   |
+                          |
+                          |
+                    =========
+                    """,
+                    """
+                      +---+
+                      |   |
+                      O   |
+                     /|   |
+                          |
+                          |
+                    =========
+                    """,
+                    """
+                      +---+
+                      |   |
+                      O   |
+                     /|\\  |
+                          |
+                          |
+                    =========
+                    """,
+                    """
+                      +---+
+                      |   |
+                      O   |
+                     /|\\  |
+                     /    |
+                          |
+                    =========
+                    """,
+                    """
+                      +---+
+                      |   |
+                      O   |
+                     /|\\  |
+                     / \\  |
+                          |
+                    =========
+                    """
+                    ]
     def __init__(self):
         pass
     
     def printSplashScreen(self):
-        print(r""" WELCOME TO....
-                 _                                             
-                | |                                            
-                | |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
-                | '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
-                | | | | (_| | | | | (_| | | | | | | (_| | | | |
-                |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
-                                    __/ |                      
-                                   |___/                       
+                print(r"""
+ _    _      _                                 
+| |  | |    | |                                
+| |  | | ___| | ___ ___  _ __ ___   ___        
+| |/\| |/ _ \ |/ __/ _ \| '_ ` _ \ / _ \       
+\  /\  /  __/ | (_| (_) | | | | | |  __/       
+ \/  \/ \___|_|\___\___/|_| |_| |_|\___|       
+                                               
+                                               
+ _____                                         
+|_   _|                                        
+  | | ___                                      
+  | |/ _ \                                     
+  | | (_) |                                    
+  \_/\___/                                     
+                                               
+                                               
+ _   _                                         
+| | | |                                        
+| |_| | __ _ _ __   __ _ _ __ ___   __ _ _ __  
+|  _  |/ _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+\_| |_/\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/                                                                  
     """)
-        while True:
-            print("########## ESC to quit ####### Press any key to continue ##########")
-            key = self.__get_key()
-            if key == "\x1b": #ESC
-                print("Quitting game!")
-                sys.exit()
-            else:
-                break
+        # while True:
+        #     print("########## ESC to quit ####### Press any key to continue ##########")
+        #     key = self.__get_key()
+        #     if key == "\x1b":   # ESC
+        #         print("Quitting game!")
+        #         sys.exit()
+        #     elif key == "\n":   # ENTER
+        #         print("Starting game...")
+        #         break
     def obtainWordLength(self):
         try:
             self.wordLen = int(input("What is the desired length of your word?: "))
             if self.wordLen <= 3:
-                print("The word must at least of four letters! Try again")
+                print("The word must be at least of four letters! Try again")
                 self.obtainWordLength()
         except Exception as  e:
             print(e)
@@ -50,15 +134,48 @@ class Game:
     
     def startMainGame(self):
         self.__api_call_for_word()
+        self.progress = ["_"]*len(self.word2guess)
+        print(f"The word has been chosen. You have {self.lives} lives left")
         self.__set_up_gallows()
-        
+        guessNum = 0
+        while(self.lives>0):
+            print("PAST GUESSES: ")
+            print(self.past_guesses)
+            guess = input(f"What is your guess #{guessNum}? ").upper()
+            self.past_guesses.append(guess)
+            if guess in list(self.word2guess):
+                
+                print("Correct!")
+                #print the filled blanks
+                for i,letter in enumerate(self.word2guess):
+                    if letter == guess:
+                        self.progress[i] = guess
+                
+            else:
+                print("Incorrect!")
+                self.lives -= 1
+                self.draw_hangman()
+                
+            if"".join(self.progress) == (self.word2guess):
+                print("CONGRATULATIONS!")
+                sys.exit()
+            else:    
+                print(" ".join(self.progress))
+                guessNum += 1
+        print(f"You are dead! The word was {self.word2guess}")
+            
+    
     
     def __get_key(self):
+        if not sys.stdin.isatty():
+            # Fallback: if not a real terminal, just use input()
+            return input("Press a key: ")[0]
+    
         fd = sys.stdin.fileno()
         old_settings = termios.tcgetattr(fd)
         try:
             tty.setraw(fd)
-            ch = sys.stdin.read(1)
+            ch = sys.stdin.read(1)  # reads one character instantly
         finally:
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
         return ch
@@ -71,7 +188,30 @@ class Game:
                 self.word2guess = response.json()[0]
         except Exception as e:
             print(e)
+            self.word2guess = "COCONUT"
         
+    def __set_up_gallows(self):
+        self.draw_hangman()
+        
+        
+    
+    def draw_hangman(self):
+        lives = self.lives
+        if (lives==6):
+            print(self.HANGMAN_PICS[0])
+        elif (lives==5):
+            print(self.HANGMAN_PICS[1])
+        elif (lives==4):
+            print(self.HANGMAN_PICS[2])
+        elif (lives==3):
+            print(self.HANGMAN_PICS[3])
+        elif (lives==2):
+            print(self.HANGMAN_PICS[4])
+        elif (lives==1):
+            print(self.HANGMAN_PICS[5])
+        elif (lives==0):
+            print(self.HANGMAN_PICS[6])
+    
 if __name__ == "__main__":
     
     game = Game()
